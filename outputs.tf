1cd8548a8c0931690b7ec083ccf4a1fb5015ef1c
Kit-Ho Mak <mcnuggets23@gmail.com>
2024-10-06 16:20:25 +0800

    remove redundant output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "cloudfront_url_endpoint" {
   value = "https://${aws_cloudfront_distribution.distribution.domain_name}"
 }
-
-output "base_url" {
-  value = aws_api_gateway_deployment.api_gateway_deployment.invoke_url
-}
